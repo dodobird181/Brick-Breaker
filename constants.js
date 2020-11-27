e18b484b96635831adc3bfcbb5af76d944593af3
Samuel Morris <dodobird181@gmail.com>
2020-11-27 10:14:21 -0800

    Add constants.js file

---
+++ constants.js
@@ -0,0 +1,8 @@
+// Settings
+export const CANVAS_WIDTH = 384
+export const CANVAS_HEIGHT = 448
+
+// Drawing
+export const BRICK_SCALING = 0.2
+export const BRICK_SOURCE_FILEPATH_START = "./res/"
+export const BRICK_COURCE_FILEPATH_END = "-Breakout-Tiles.png"
