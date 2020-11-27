b3df8945a99351ea657ad78508bcbefeae3b1445
Samuel Morris <dodobird181@gmail.com>
2020-11-27 11:42:10 -0800

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,8 +1,22 @@
-// Settings
+// Canvas Settings
 export const CANVAS_WIDTH = 384
 export const CANVAS_HEIGHT = 448
 
-// Drawing
+// Ball(s) Settings
+export const BALL_SPEED = 3
+
+// Drawing Settings
 export const BRICK_SCALING = 0.2
 export const BRICK_SOURCE_FILEPATH_START = "./res/"
 export const BRICK_COURCE_FILEPATH_END = "-Breakout-Tiles.png"
+
+// Player Settings
+export const PLAYER_START_HEIGHT = 50
+export const PLAYER_SCALING = 0.15
+export const PLAYER_SPEED = 3.5
+
+// Keys
+export const KEY_LEFT = 37
+export const KEY_RIGHT = 39
+export const KEY_A = 65
+export const KEY_D = 68
