687eae0dbf46194bb46eca08387ae5e475484a6f
Samuel Morris <dodobird181@gmail.com>
2020-12-25 18:36:59 -0800

    Create RGBCol class for easier color manipulation

---
+++ colors.js
@@ -0,0 +1,20 @@
+import { RangedInt } from "./utils/ranged_int.js"
+
+export class RGBCol{
+    constructor(r, g, b, a=1.0){
+        this.r = new RangedInt(r, 0, 255)
+        this.g = new RangedInt(g, 0, 255)
+        this.b = new RangedInt(b, 0, 255)
+        this.a = a
+    }
+
+    toString(){
+        return "rgb(" + this.r.get() + ", " + this.g.get() + ", " + this.b.get() + ", " + this.a +")"
+    }
+
+    darken(amt){
+        this.r.set(this.r.get() - amt)
+        this.g.set(this.g.get() - amt)
+        this.b.set(this.b.get() - amt)
+    }
+}
