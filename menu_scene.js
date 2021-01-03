ecb6029f2d4337b964b1909f835b04958af367c5
Samuel Morris <dodobird181@gmail.com>
2021-01-03 10:50:07 -0800

    Changed name of "info" button to "credits"

--- menu_scene.js
+++ menu_scene.js
@@ -1,6 +1,6 @@
 import { CANVAS_HEIGHT, CANVAS_WIDTH, S_CLICK, S_HOVER, S_LEVEL_WIN, S_MENU } from "./constants.js";
 import { LevelSelectionScene } from "./level_selection_scene.js";
-import { ctx, gameScene, initilizeMenuButton, loadScene, menuScene } from "./main.js";
+import { ctx, gameScene, hideElement, initilizeMenuButton, loadScene, menuScene } from "./main.js";
 import { Track, playSound } from "./sound.js";
 import { getTextWidth, Text } from "./utils/text.js";
 
@@ -11,8 +11,8 @@ export class MenuScene{
         this.gameStarted = false
         this.music = new Track(S_MENU)
         this.menuElement = document.getElementById("menuElement")
-        this.levelSelectionElement = document.getElementById("levelSelectionElement")
-        this.levelSelectionElement.style.display = "none"
+        hideElement("levelSelectionElement")
+        hideElement("gameMessageElement")
     }
 
     draw(){
@@ -36,12 +36,12 @@ export class MenuScene{
     startMenu(event){
         if (!menuScene.gameStarted){
             playSound(S_CLICK)
-            menuScene.music.play(0.2)
+            menuScene.music.play(0.05)
             menuScene.gameStarted = true
 
             var startButton = document.getElementById("startButton")
             var levelSelectionButton = document.getElementById("levelSelectionButton")
-            var infoButton = document.getElementById("infoButton")
+            var creditsButton = document.getElementById("creditsButton")
 
             // Start
             initilizeMenuButton(startButton, () => {
@@ -55,9 +55,9 @@ export class MenuScene{
                 loadScene(new LevelSelectionScene())
             })
 
-            // Info
-            initilizeMenuButton(infoButton, () => {
-
+            // Credits
+            initilizeMenuButton(creditsButton, () => {
+                loadScene(new CreditsScene())
             })
         }
     }
